8ba92f1fe2fc33fe65acb9cd3d5750f2050cff59
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-31 15:58:09 +0100

    ajouter seeders

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->insert(
+            [
+                'name'=> Str::random(10),
+                'price'=> random_int(100,1000),
+                'description' =>Str::random(10),
+                
+            ]
+
+        );
+    }
+}
