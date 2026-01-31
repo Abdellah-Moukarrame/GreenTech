ba93abb0864a53be45eb96d46d5b6b74ed4a5a23
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-31 15:59:10 +0100

    ajouter routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
+Route::get('/admin/create', [ProductController::class, 'create']);
+Route::post('/admin/create', [ProductController::class, 'store']);
+Route::get('/admin/create', [CategoryController::class, 'index']);
+Route::delete('/admin/delete/{id}',[ProductController::class,'destroy'])->name('products.destroy');
+Route::get('/admin/update/{id}',[ProductController::class,'edit']);
+Route::post('/admin/update/{id}',[ProductController::class,'update']);
