e9788c6c616966d03b0e4e824dddf3d03e9d3067
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-09 23:56:50 +0100

    client controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Favorites;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    public function index(){
-        return view('client.clientdash');
 
+    public function index()
+    {
+
+        $products = Product::all();
+        $categories = Category::all();
+        $favorites = Favorites::all();
+        return view('client.clientdash', compact('products', 'categories','favorites'));
     }
 }
