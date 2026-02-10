c9c54e1860f097765b4b56a25eb87c4d25f48356
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-10 12:04:02 +0100

    FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Favorites;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoritesController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        $categories = Category::all();
+        $favorites = Favorites::all()->where('user_id',Auth::user()->id);
+        return view('client.favorites',compact('favorites','products','categories'));
+
+    }
+    public function create(Request $request)
+    {
+        Favorites::create(
+            [
+                "user_id"=>Auth::user()->id,
+                "product_id"=>$request->product_id
+            ]
+        );
+        return redirect()->route('client.favorites');
+    }
 }
