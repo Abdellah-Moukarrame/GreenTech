29632c844290a167729c805452f83cfacba3c52a
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-10 12:05:52 +0100

    add middlewares

--- app/Http/Middleware/ClientMiddleware.php
+++ app/Http/Middleware/ClientMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClientMiddleware
@@ -15,6 +16,9 @@ class ClientMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role =="client") {
+           return $next($request);
+        }
+        return back();
     }
 }
